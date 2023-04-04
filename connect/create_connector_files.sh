3f08b6f5d0d0ed60288ec6ecec4249a3d5d08fbb
Maygol Kananizadeh <mkananizadeh@confluent.io>
2023-04-04 11:01:22 -0700

    mkananizadeh: Added connect directory to automatically create connector config files in v2.0

---
+++ connect/create_connector_files.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo "generating connector configuration json files from .env"
+echo
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../.env
+
+for i in example_postgres_products_source.json example_postgres_customers_source.json example_snowflake_sink.json; do
+    sed -e "s|<add_your_api_key>|${CCLOUD_API_KEY}|g" \
+    -e "s|<add_your_api_secret_key>|${CCLOUD_API_SECRET}|g" \
+    -e "s|<add_your_postgres_products_hostname>|${POSTGRES_PRODUCTS_ENDPOINT}|g" \
+    -e "s|<add_your_postgres_customers_hostname>|${POSTGRES_CUSTOMERS_ENDPOINT}|g" \
+    -e "s|<add_snowflake_private_key>|${SF_PVT_KEY}|g" \
+    -e "s|<add_snowflake_account>|${SNOWFLAKE_ACCOUNT}|g" \
+    ${DIR}/$i > ${DIR}/actual_${i#example_}
+done
