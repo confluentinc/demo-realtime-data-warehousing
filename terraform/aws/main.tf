f690f02e8f7a353f2d60d2a2543e12932b573091
Maygol Kananizadeh <mkananizadeh@confluent.io>
2023-04-04 10:59:44 -0700

    mkananizadeh: Updated Terraform plan for aws/main.tf for v2.0

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,14 +1,235 @@
 terraform {
-    required_providers {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.17.0"
+    }
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.58.0"
+    }
   }
 }
-provider "aws" {
-    region = "us-west-2"
+
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
 }
+
+provider "confluent" {
+  # https://developer.hashicorp.com/terraform/language/providers/configuration#alias-multiple-provider-configurations
+  alias = "kafka"
+
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+
+  #   kafka_id            = confluent_kafka_cluster.basic.id
+  kafka_rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  kafka_api_key       = confluent_api_key.app-manager-kafka-api-key.id
+  kafka_api_secret    = confluent_api_key.app-manager-kafka-api-key.secret
+}
+
+provider "aws" {
+  region = var.region
+}
+
+provider "snowflake" {
+  alias = "sys_admin"
+  role  = "SYSADMIN"
+}
+
+provider "snowflake" {
+  alias = "security_admin"
+  role  = "SECURITYADMIN"
+}
+
+resource "confluent_environment" "demo" {
+  display_name = "Demo_Real_Time_Data_Warehousing"
+}
+
+data "confluent_schema_registry_region" "essentials" {
+  cloud   = "AWS"
+  region  = "us-east-2"
+  package = "ESSENTIALS"
+}
+
+resource "confluent_schema_registry_cluster" "essentials" {
+  package = data.confluent_schema_registry_region.essentials.package
+
+  environment {
+    id = confluent_environment.demo.id
+  }
+
+  region {
+    # See https://docs.confluent.io/cloud/current/stream-governance/packages.html#stream-governance-regions
+    id = data.confluent_schema_registry_region.essentials.id
+  }
+}
+
+resource "confluent_kafka_cluster" "basic" {
+  display_name = "demo_kafka_cluster"
+  availability = "SINGLE_ZONE"
+  cloud        = "AWS"
+  region       = "us-west-2"
+  basic {}
+
+  environment {
+    id = confluent_environment.demo.id
+  }
+}
+
+# 'app-manager' service account is required in this configuration to create 'postgres.customers.customers' topic and grant ACLs
+# to 'app-producer' and 'app-consumer' service accounts.
+resource "confluent_service_account" "app-manager" {
+  display_name = "app-manager"
+  description  = "Service account to manage 'demo' Kafka cluster"
+}
+
+resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
+  principal   = "User:${confluent_service_account.app-manager.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.basic.rbac_crn
+}
+
+resource "confluent_api_key" "app-manager-kafka-api-key" {
+  display_name = "app-manager-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-manager' service account"
+  owner {
+    id          = confluent_service_account.app-manager.id
+    api_version = confluent_service_account.app-manager.api_version
+    kind        = confluent_service_account.app-manager.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.basic.id
+    api_version = confluent_kafka_cluster.basic.api_version
+    kind        = confluent_kafka_cluster.basic.kind
+
+    environment {
+      id = confluent_environment.demo.id
+    }
+  }
+
+  # The goal is to ensure that confluent_role_binding.app-manager-kafka-cluster-admin is created before
+  # confluent_api_key.app-manager-kafka-api-key is used to create instances of
+  # confluent_kafka_topic, confluent_kafka_acl resources.
+
+  # 'depends_on' meta-argument is specified in confluent_api_key.app-manager-kafka-api-key to avoid having
+  # multiple copies of this definition in the configuration which would happen if we specify it in
+  # confluent_kafka_topic, confluent_kafka_acl resources instead.
+  depends_on = [
+    confluent_role_binding.app-manager-kafka-cluster-admin
+  ]
+}
+
+# Create postgres.customers.customers topic 
+resource "confluent_kafka_topic" "postgres_customers_customers" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+
+  topic_name       = "postgres.customers.customers"
+  rest_endpoint    = confluent_kafka_cluster.basic.rest_endpoint
+  partitions_count = 1
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+# Create postgres.customers.demographics topic 
+resource "confluent_kafka_topic" "postgres_customers_demographics" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+
+  topic_name       = "postgres.customers.demographics"
+  rest_endpoint    = confluent_kafka_cluster.basic.rest_endpoint
+  partitions_count = 1
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+# Create postgres.products.products topic 
+resource "confluent_kafka_topic" "postgres_products_products" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+
+  topic_name       = "postgres.products.products"
+  rest_endpoint    = confluent_kafka_cluster.basic.rest_endpoint
+  partitions_count = 1
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+# Create postgres.products.orders topic 
+resource "confluent_kafka_topic" "postgres_products_orders" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+
+  topic_name       = "postgres.products.orders"
+  rest_endpoint    = confluent_kafka_cluster.basic.rest_endpoint
+  partitions_count = 1
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+
+# Create a service account for ksqlDB 
+resource "confluent_service_account" "app-ksql" {
+  display_name = "app-ksql"
+  description  = "Service account to manage 'demo-ksql' ksqlDB cluster"
+}
+
+resource "confluent_role_binding" "app-ksql-kafka-cluster-admin" {
+  principal   = "User:${confluent_service_account.app-ksql.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.basic.rbac_crn
+}
+
+resource "confluent_role_binding" "app-ksql-schema-registry-resource-owner" {
+  principal   = "User:${confluent_service_account.app-ksql.id}"
+  role_name   = "ResourceOwner"
+  crn_pattern = format("%s/%s", confluent_schema_registry_cluster.essentials.resource_name, "subject=*")
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+# Create ksqlDB cluster  
+resource "confluent_ksql_cluster" "demo-ksql" {
+  display_name = "demo-ksql"
+  csu          = 1
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  credential_identity {
+    id = confluent_service_account.app-ksql.id
+  }
+  environment {
+    id = confluent_environment.demo.id
+  }
+  depends_on = [
+    confluent_role_binding.app-ksql-kafka-cluster-admin,
+    confluent_role_binding.app-ksql-schema-registry-resource-owner,
+    confluent_schema_registry_cluster.essentials
+  ]
+}
+
+
 resource "aws_default_vpc" "default_vpc" {
     tags = {
         name = "Default VPC"
@@ -70,9 +291,7 @@ resource "aws_eip" "postgres_customers_ip" {
         created_by = "terraform"
     }
 }
-output "postgres_instance_customers_public_endpoint" {
-    value = aws_eip.postgres_customers_ip.public_ip
-}
+
 data "template_cloudinit_config" "pg_bootstrap_products" {
     base64_encode = true
     part {
@@ -98,6 +317,134 @@ resource "aws_eip" "postgres_products_ip" {
         created_by = "terraform"
     }
 }
-output "postgres_instance_products_public_endpoint" {
-    value = aws_eip.postgres_products_ip.public_ip
+
+
+resource "snowflake_database" "db" {
+  provider = snowflake.security_admin
+  name     = "TF_DEMO"
+}
+
+resource "snowflake_warehouse" "warehouse" {
+  provider                            = snowflake.sys_admin
+  name                                = "TF_DEMO"
+  warehouse_size                      = "X-SMALL"
+  query_acceleration_max_scale_factor = 0
+  auto_suspend                        = 60
+}
+
+resource "snowflake_role" "role" {
+  provider = snowflake.security_admin
+  name     = "TF_DEMO_SVC_ROLE"
+}
+
+resource "snowflake_database_grant" "grant_usage" {
+  provider      = snowflake.security_admin
+  database_name = snowflake_database.db.name
+  privilege     = "USAGE"
+  roles         = [snowflake_role.role.name]
+}
+
+resource "snowflake_schema_grant" "usage" {
+  provider      = snowflake.security_admin
+  database_name = snowflake_database.db.name
+  schema_name = var.snowflake_schema
+  privilege   = "USAGE"
+  roles       = [snowflake_role.role.name]
+}
+
+resource "snowflake_schema_grant" "grant_write" {
+  provider      = snowflake.security_admin
+  database_name = snowflake_database.db.name
+  privilege     = "CREATE TABLE"
+  roles         = [snowflake_role.role.name]
+  schema_name   = var.snowflake_schema
+}
+
+resource "snowflake_schema_grant" "create_stage" {
+  provider      = snowflake.security_admin
+  database_name = snowflake_database.db.name
+  privilege     = "CREATE STAGE"
+  roles         = [snowflake_role.role.name]
+  schema_name   = var.snowflake_schema
+}
+
+resource "snowflake_schema_grant" "create_pipe" {
+  provider      = snowflake.security_admin
+  database_name = snowflake_database.db.name
+  privilege     = "CREATE PIPE"
+  roles         = [snowflake_role.role.name]
+  schema_name   = var.snowflake_schema
+}
+
+resource "snowflake_table_grant" "select" {
+  provider      = snowflake.security_admin
+  database_name = snowflake_database.db.name
+  schema_name   = var.snowflake_schema
+  privilege = "SELECT"
+  roles     = [snowflake_role.role.name]
+  on_future = true
+}
+
+resource "snowflake_table_grant" "insert" {
+  provider      = snowflake.security_admin
+  database_name = snowflake_database.db.name
+  schema_name   = var.snowflake_schema
+  privilege = "INSERT"
+  roles     = [snowflake_role.role.name]
+  on_future = true
+}
+
+resource "snowflake_table_grant" "update" {
+  provider      = snowflake.security_admin
+  database_name = snowflake_database.db.name
+  schema_name   = var.snowflake_schema
+  privilege = "UPDATE"
+  roles     = [snowflake_role.role.name]
+  on_future = true
+}
+
+resource "snowflake_table_grant" "delete" {
+  provider      = snowflake.security_admin
+  database_name = snowflake_database.db.name
+  schema_name   = var.snowflake_schema
+  privilege = "DELETE"
+  roles     = [snowflake_role.role.name]
+  on_future = true
+}
+
+resource "snowflake_warehouse_grant" "grant_usage" {
+  provider       = snowflake.security_admin
+  warehouse_name = snowflake_warehouse.warehouse.name
+  privilege      = "USAGE"
+  roles          = [snowflake_role.role.name]
+}
+
+resource "snowflake_warehouse_grant" "grant_operate" {
+  provider       = snowflake.security_admin
+  warehouse_name = snowflake_warehouse.warehouse.name
+  privilege      = "OPERATE"
+  roles          = [snowflake_role.role.name]
+}
+
+resource "tls_private_key" "svc_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "snowflake_user" "user" {
+  provider          = snowflake.security_admin
+  name              = "TF_DEMO_USER"
+  default_warehouse = snowflake_warehouse.warehouse.name
+  default_role      = snowflake_role.role.name
+  rsa_public_key = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
+}
+
+
+resource "snowflake_role_grants" "grants" {
+  provider  = snowflake.security_admin
+  role_name = snowflake_role.role.name
+  roles = [
+    "SECURITYADMIN"
+  ]
+  users = [snowflake_user.user.name]
 }
